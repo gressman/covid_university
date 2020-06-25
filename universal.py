72a023027ffa1d79fc0fc79bc9ccd5e7f529f61a
gressman <67444376+gressman@users.noreply.github.com>
2020-06-25 19:40:55 -0400

    Update universal.py

--- universal.py
+++ universal.py
@@ -1,3 +1,19 @@
+#    universal.py : Global Constants for Simulation Scenarios
+#    Copyright (C) 2020 Philip T. Gressman <gresssman@math.upenn.edu> and Jennifer R. Peck <jpeck1@swarthmore.edu>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import probtools
 
 
